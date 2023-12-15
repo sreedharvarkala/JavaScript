a1c8d7b2e84066917129f6181262bf665bcfd4b8
sreedharvarkala <153958346+sreedharvarkala@users.noreply.github.com>
2023-12-16 04:16:54 +0530

    Add files via upload

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 14453;
+let accountEmail = "sreedhar@google.com";
+var accountPassword = "12345";
+accountCity = "Hyderbad";
+let accountState;
+
+// accountId = 2;
+accountEmail = "hc@hc.com";
+accountPassword = "9441414141";
+accountCity = "bengaluru";
+
+// console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+console.log(accountState);
---
+++ 01_basics/letdemo.js
@@ -0,0 +1,7 @@
+var x = 1;
+
+if (x === 1) {
+  var x = 2;
+  console.log(x);
+}
+console.log(x);
